db25c849ab03dc0a89f481372524e37fc6760c31
mackenzie <mackenziewangenstein@gmail.com>
2018-03-07 09:29:57 -0800

    fixed accuracy

--- hw3/bayesclassifier.py
+++ hw3/bayesclassifier.py
@@ -24,11 +24,13 @@ class BayesClassifier(object):
 		self.not_spam_std_devs = []
 		self.test_set_labeled = np.concatenate((spam_train, not_spam_train), axis=0)
 		self.test_set = self.test_set_labeled[:,0:self.feature_count]
-		self.test_confusion_matrix = np.zeros((self.test_set.shape[0], self.test_set.shape[0]))
+		self.test_confusion_matrix = np.zeros((2,2))
 		print("confusion matrix shape: ", self.test_confusion_matrix.shape)
+		print("feature count: ", self.feature_count)
 		# print("Test_set shape ", self.test_set.shape)
 		# self.exceptions_caught = 0
 		# self.none_exceptions = 0
+
 
 	def predict(self):
 		# PART 2
@@ -36,78 +38,70 @@ class BayesClassifier(object):
 		print("test of std devs and means")
 		print("spam mean for 57 features ", self.spam_means)
 		print("spam std dev for ", self.spam_std_devs.shape, " features: ", self.spam_std_devs)
-		print("spam mean for 57 features ", self.not_spam_means)
-		print("spam std dev for 57 features: ", self.not_spam_std_devs)  # TODO: keep or remove
+		print("not spam mean for 57 features ", self.not_spam_means)
+		print("not spam std dev for 57 features: ", self.not_spam_std_devs)  # TODO: keep or remove
 
+		test_prediction_results, invalid_predictions = self.predict_class()
+		_test_accuracy = putil.compute_accuracy(self.test_confusion_matrix)
+		print(_test_accuracy)
 		# Part 3) Use Gaussian Naive Bayes algorith to classify test set
-		return self.predict_class()
+		return test_prediction_results, self.test_confusion_matrix, _test_accuracy, invalid_predictions
+
 
 	def calc_means_and_std_devs(self):
 		# get mean and std devations for spam features
-		self.spam_means = np.sum(self.spam_train,
-								 axis=0) / self.spam_train_count  # for each featurea add up all rows and divide by spam count
-		_mean_spam_matrix = np.repeat(self.spam_means[np.newaxis, :], self.spam_train.shape[0],
-									  0)  # reapet mean row to match shape of spam
-		spam_std_dev_inner = np.square(
-			self.spam_train - _mean_spam_matrix)  # (fi - mi)^2 4each feat:shape[data, feature]
-		self.spam_std_devs = np.sqrt(
-			spam_std_dev_inner.sum(axis=0) / self.spam_train_count)  # get std dev for each feature for spam
+
+		# for each featurea add up all rows and divide by spam count
+		self.spam_means = np.sum(self.spam_train, axis=0) / self.spam_train_count
+		_mean_spam_matrix = np.repeat(self.spam_means[np.newaxis, :], self.spam_train.shape[0],0)  # repeaet mean row
+		spam_std_dev_inner = np.square(	self.spam_train - _mean_spam_matrix)  # (fi - mi)^2 4each feat:shape[data,feat]
+		self.spam_std_devs = np.sqrt(	spam_std_dev_inner.sum(axis=0) / self.spam_train_count)  #std dev 4each feature
 		self.spam_std_devs[self.spam_std_devs == 0] = 0.0001
 
-		# get mean and std devations for not spam features
-		self.not_spam_means = np.sum(self.not_spam_train,
-									 axis=0) / self.not_spam_train_count  # for each featurea add up all rows and divide by spam count
-		not_mean_spam_matrix = np.repeat(self.not_spam_means[np.newaxis, :], self.not_spam_train.shape[0],
-										 0)  # reapet mean row to match shape of spam
+		self.not_spam_means = np.sum(self.not_spam_train, axis=0) / self.not_spam_train_count  #
+		not_mean_spam_matrix = np.repeat(self.not_spam_means[np.newaxis, :], self.not_spam_train.shape[0],0)
 		not_spam_std_dev_inner = np.square(self.not_spam_train - not_mean_spam_matrix)
-		self.not_spam_std_devs = np.sqrt(
-			not_spam_std_dev_inner.sum(axis=0) / self.not_spam_train_count)  # get std dev for each feature for spam
+		self.not_spam_std_devs = np.sqrt(not_spam_std_dev_inner.sum(axis=0) / self.not_spam_train_count)
 		self.not_spam_std_devs[self.not_spam_std_devs == 0] = 0.0001
 
 	def predict_class(self):
 		#create a dict to hold the probability of spam, not spam, and class predicted
-		# convert each
-		print("test of shape for test set: ", self.test_set.shape)
 		test_prediction_results = dict()
-		for data_idx in range(self.test_set.shape[0]):
+		invalid_predictions = dict()
+		for data_idx in range(self.test_set_labeled.shape[0]):
 			spam_feature_probs = np.zeros(self.feature_count)
 			not_spam_feature_probs = np.zeros(self.feature_count)
 			for feature_pos in range(self.feature_count):
-				x_sub = self.test_set[data_idx][feature_pos]
+				x_sub = self.test_set_labeled[data_idx][feature_pos]
 				spam_feature_probs[feature_pos] = self.gaussian_naive_bayes_alg(x_sub,
 																					self.spam_means[feature_pos],
 																					self.spam_std_devs[feature_pos])
 				not_spam_feature_probs[feature_pos] = self.gaussian_naive_bayes_alg(x_sub,
 																					self.not_spam_means[feature_pos],
 																					self.not_spam_std_devs[feature_pos])
-				# print("norm features distrubted post", spam_feature_probs)
-				# print("norm features distrubted neg", not_spam_feature_probs)
-				# print("numbers of exceptions caught: ", self.exceptions_caught)
-				# print("non exceptions ", self.none_exceptions)
-			prob_of_spam = math.log(self.prob_spam)*np.sum(spam_feature_probs)
-			# print("prob of spam: ", prob_of_spam)
-			prob_of_not_spam = math.log(self.prob_not_spam)*np.sum(not_spam_feature_probs)
-			# print("prob of not spam: ", prob_of_not_spam)
+				# print("shape of spam probs: ", spam_feature_probs)
+			prob_of_spam = math.log(self.prob_spam)+np.sum(spam_feature_probs)
+			prob_of_not_spam = math.log(self.prob_not_spam)+np.sum(not_spam_feature_probs)
 
 			_predicted = 0
 			if prob_of_spam > prob_of_not_spam:
 				_predicted = 1
-				#np.argmax([prob_of_spam, prob_of_not_spam])
-			_actual = int(self.test_set_labeled[data_idx][57]) #TODO: change from hard coded
-			print("prediction: ", _predicted, "actual: ", _actual)
+			_actual = int(self.test_set_labeled[data_idx][self.feature_count])
 			self.test_confusion_matrix[_predicted][_actual] += 1
-		_test_accuracy = putil.compute_accuracy(self.test_confusion_matrix)
+			test_prediction_results[data_idx] = {
+				'prob_spam': prob_of_spam,
+				'prob_not_spam': prob_of_not_spam,
+				'predicted': _predicted ,
+				'actual': _actual}
 
-
-		test_prediction_results[data_idx] = {
-			'prob_spam': prob_of_spam,
-			'prob_not_spam': prob_of_not_spam,
-			'predicted': _predicted ,
-			'actual': _actual
-		}
-
-
-		return test_prediction_results, self.test_confusion_matrix, _test_accuracy
+			if _actual != _predicted:
+				invalid_predictions[data_idx]  = {
+					'prob_spam': prob_of_spam,
+					'prob_not_spam': prob_of_not_spam,
+					'predicted': _predicted,
+					'actual': _actual
+				}
+		return test_prediction_results, invalid_predictions
 
 
 	def gaussian_naive_bayes_alg(self, x_sub, mean, std_dev):
@@ -119,12 +113,11 @@ class BayesClassifier(object):
 		:return: log(P(x_sub_i | class_sub_j)
 		"""
 		scalar = 1 / (math.sqrt(2 * math.pi) * std_dev)
-		# print("scalar: ", scalar)
 		inner = (math.pow(x_sub - mean, 2) / (2 * pow(std_dev, 2)))
-		# print("inner: ", inner)
 		exp_eval = math.exp(-inner)
 		if exp_eval == 0:
-			exp_eval = np.nextafter(1 / inner, 1)
-		# print("exp val: ", exp_eval )
-		N =  math.log(scalar *exp_eval)
-		return N
+			exp_eval = np.nextafter(1/math.pow(inner,13), 1)
+		return math.log(scalar* exp_eval) #TODO: put back in multiplication of constant
+		#with constant accuracy is around 70%, without it its around 82% which is what my peers are getting
+
+
--- hw3/bayesrunner.py
+++ hw3/bayesrunner.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
 import math
+import pprint
 
 from hw3.bayesclassifier import BayesClassifier
+
 
 def run():
 	data = np.array(pd.read_csv("hw3/spambase.csv"))  # dataset.values returns an np array
 	print("data.shape ", data.shape)
 	_data_spam = data[data[:, 57] > 0, :]
+	print(_data_spam[_data_spam[:, 56] == 278, :])
 	# _data_spam = _data_spam[:, 0:_data_spam.shape[1]-1]
 
 
@@ -15,7 +18,6 @@ def run():
 	# _data_not_spam = _data_not_spam[:, 0:_data_not_spam.shape[1]-1]
 	np.random.shuffle(_data_spam)
 	np.random.shuffle(_data_not_spam)
-
 
 	spam_split_ind = math.floor(_data_spam.shape[0] / 2)
 	spam_train, spam_test = _data_spam[:spam_split_ind, ], _data_spam[spam_split_ind:]
@@ -28,10 +30,12 @@ def run():
 	print("shape of  not spam_test: ", not_spam_test.shape)
 
 	bc = BayesClassifier(spam_train, spam_test, not_spam_train, not_spam_test)
-	results, confusion_matrix, accuracy = bc.predict()
+	results, confusion_matrix, accuracy, invalid_predictions = bc.predict()
 	print("accuracy: ", accuracy)
-	print(results)
-	print(confusion_matrix)
+	print("\n", confusion_matrix)
+	print("Printing invalid predictions")
+	print(invalid_predictions)
+	# print_results(results)
 
 
 def test_means_and_dev_calc():
@@ -45,15 +49,31 @@ def test_means_and_dev_calc():
 	print("test shape: ", test_features_matrix.shape)
 	mean_spam = np.sum(test_features_matrix, axis=0) / test_features_matrix.shape[0]
 	print("test of mean\n", mean_spam)
-	mean_spam_matrix = np.repeat(mean_spam[np.newaxis, :],test_features_matrix.shape[0], 0)
+	mean_spam_matrix = np.repeat(mean_spam[np.newaxis, :], test_features_matrix.shape[0], 0)
 	print("test of means matrix \n", mean_spam_matrix)
 
-	#combined into two lines
+	# combined into two lines
 	std_dev_inner = np.square(test_features_matrix - mean_spam_matrix)
-	std_dev = np.sqrt(std_dev_inner.sum(axis=0)/test_features_matrix.shape[0])
+	std_dev = np.sqrt(std_dev_inner.sum(axis=0) / test_features_matrix.shape[0])
 	print("two steps ", std_dev)
 
+	not_spam_test_features_matrix = np.array([[2.0, 1.1], [4.1, 2.0], [8.1, 9.4]])
+	not_mean_spam = np.sum(not_spam_test_features_matrix, axis=0) / not_spam_test_features_matrix.shape[0]
+	not_mean_spam_matrix = np.repeat(not_mean_spam[np.newaxis, :], not_spam_test_features_matrix.shape[0], 0)
+	not_std_dev_inner = np.square(not_spam_test_features_matrix - not_mean_spam_matrix)
+	not_std_dev = np.sqrt(not_std_dev_inner.sum(axis=0) / not_spam_test_features_matrix.shape[0])
+
+	print("test of not spam mean\n", not_mean_spam)
+	print("test of not spam std ", not_std_dev)
+	print("test features matrix\n", test_features_matrix)
+
+	col_labels = np.ones((test_features_matrix.shape[0], 1))
+	test_features_matrix = np.append(test_features_matrix, col_labels, axis=1)
+	print("test features matrix\n", test_features_matrix)
+	col_labels_zero =  np.zeros((not_spam_test_features_matrix.shape[0], 1))
+	not_spam_test_features_matrix = np.append(not_spam_test_features_matrix,col_labels_zero, axis=1)
+	print("testnot spam features matrix\n", not_spam_test_features_matrix)
 
 
-
-
+def print_results(results_dict):
+	pprint.pprint(results_dict)
